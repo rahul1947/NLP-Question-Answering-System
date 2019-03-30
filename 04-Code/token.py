d0986f702a1907bfeaf320407dc37e693cc13bba
Rahul Nalawade <rahul.nalawade@utdallas.edu>
2019-03-29 23:43:54 -0500

    Lemmatize, POS, Dependency Parsing, Named Entity

--- 04-Code/token.py
+++ 04-Code/token.py
@@ -14,21 +14,64 @@ def read_data(filepath):
     return corpus
 
 # Creates an array of tokens for the given corpus.
-def tokennize(corpus):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(corpus)
+def tokennize(doc):
     
-    [token.text for token in doc]
-    tokens = [token.text for token in doc]
+    #[token.text for token in doc]
+    tokens = [word.text for word in doc]
     return tokens
+
+# Lemmatizes corpus to extract lemmas as features
+def lemmatize(doc):
+    
+    lemmas = [word.lemma_ for word in doc]
+    return lemmas
+
+# Gives Part-Of-Speech tags for each word in corpus.
+def getPOS(doc):
+    
+    pos_tags = [word.pos_ for word in doc]
+    return pos_tags
+
+# Provides Syntactic Dependencies 
+def synParsing(doc):
+    
+    # Dependency Labels
+    dependC = [word.dep_ for word in doc]
+    
+    # Syntactic head token (governor)
+    heads = [word.head.text for word in doc]
+    
+    return dependC, heads
+
+def getNamedEntities(doc):
+    
+    # Text and label of named entity span
+    namedEntities = [(ent.text, ent.label_) for ent in doc.ents]
+    
+    return namedEntities
+
 
 # Main function
 if __name__ == '__main__':
 
     filepath = '../02-Project-Data/WikipediaArticles/AbrahamLincoln.txt'    
     corpus = read_data(filepath)
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(corpus)
+    
     #print(corpus)
-    tokens = tokennize(corpus)
-    print(tokens)
-
+    tokens = tokennize(doc)
+    #print(tokens)
+    lemmas = lemmatize(doc)
+    #print(lemmas)
+    pos_tags = getPOS(doc)
+    #print(pos_tags)
+    
+    dependC, heads = synParsing(doc)
+    #print(dependC)
+    #print(heads)
+    
+    namedEntity = getNamedEntities(doc)
+    print(namedEntity)
+    
 #The-End
