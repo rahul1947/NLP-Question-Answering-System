af5083ae2ec615fede0f79e3f01eed6f2928e541
Rahul Nalawade <rahul.nalawade@utdallas.edu>
2019-03-29 21:24:32 -0500

    Tokennizer: Done

--- 04-Code/token.py
+++ 04-Code/token.py
@@ -7,11 +7,28 @@ Created on Fri Mar 29 20:45:43 2019
 """
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a text")
+# Reads data from the given file directed as in filepath, and returns it.
+def read_data(filepath):
+    f = open(filepath, 'r')
+    corpus = f.read()
+    return corpus
 
-[token.text for token in doc]
-#tokens = [token.text for token in doc]
-#print(tokens)
+# Creates an array of tokens for the given corpus.
+def tokennize(corpus):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(corpus)
+    
+    [token.text for token in doc]
+    tokens = [token.text for token in doc]
+    return tokens
 
+# Main function
+if __name__ == '__main__':
 
+    filepath = '../02-Project-Data/WikipediaArticles/AbrahamLincoln.txt'    
+    corpus = read_data(filepath)
+    #print(corpus)
+    tokens = tokennize(corpus)
+    print(tokens)
+
+#The-End
