9a98d33675a9a5b02ab6b371989ed55c8a960517
Rahul Nalawade <rahul.nalawade@utdallas.edu>
2019-03-29 20:58:56 -0500

    Spacy: Test Program

--- 04-Code/token.py
+++ 04-Code/token.py
@@ -10,6 +10,8 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("This is a text")
 
-tokens = [token.text for token in doc]
+[token.text for token in doc]
+#tokens = [token.text for token in doc]
+#print(tokens)
 
-print(tokens)
+
